a93bf2184a785afa4e99827c13f9c4b0218d9f46
Avinandan khanda <avinandankhanda2002>
2019-05-31 19:12:36 +0530

    new_file

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+words={
+    "word":'bellygod',
+    "definition":'over-weight',
+    "language":"english"
+}
+# accessing:
+print(words["word"])
+# Updating
+print(words)
+for i in words.keys():
+    print(i)
+for b in words.values():
+    print(b)
