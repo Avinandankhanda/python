3412250aa87ae8f85eb8fe073467c1495c0f55f5
Avinandan khanda <avinandankhanda2002>
2019-06-16 21:37:40 +0530

    new file

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class cricketer():
+    def __init__(self,team,name):
+        self.team=team
+        self.name=name
+aa=cricketer("india",'m.s')
+ab=cricketer('bangladesh','r.s')
+ac=cricketer('england','s.s')
+print(aa.team)
+print(aa.name)
+print(ab.name)
+print(ab.team)
+print(ac.team)
+print(ac.name)
